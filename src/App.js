577bd10340ca1b75728306d39757826271628c2e
Yab1 <yabilisanu@gmail.com>
2023-04-20 21:35:40 +0300

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
