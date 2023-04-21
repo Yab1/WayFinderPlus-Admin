75f73b37dbb3f3d9500e3ea128b6fae3d23adac3
Yab1 <yabilisanu@gmail.com>
2023-04-21 23:41:09 +0300

    Create a template for rendering tools

---
+++ src/components/Drawer/tool.js
@@ -0,0 +1,51 @@
+// MUI Components
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+
+//MUI Icons
+import EventIcon from "@mui/icons-material/Event";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import FeedIcon from "@mui/icons-material/Feed";
+
+// MUI Colors
+import { grey } from "@mui/material/colors";
+
+const drawerWidth = 60;
+const drawerHeight = "94%";
+
+export default function Tool({ icon: { title, iconMUI, key } }) {
+  // Functions
+  const changeWindow = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <Tooltip title={title} placement="right" disableInteractive>
+      <IconButton
+        key={key}
+        onClick={changeWindow}
+        sx={{
+          justifyContent: "flex-start",
+          color: grey[50],
+          width: 65,
+          // my: 1,
+          marginLeft: 3,
+          borderTopLeftRadius: 40,
+          borderBottomLeftRadius: 40,
+          transition: "background 600ms linear,color 250ms linear",
+          "&:hover": {
+            bgcolor: "transparent",
+            color: grey[900],
+          },
+          "&:focus": {
+            bgcolor: "#ffffff",
+            color: grey[900],
+          },
+        }}
+      >
+        {iconMUI}
+      </IconButton>
+    </Tooltip>
+  );
+}
