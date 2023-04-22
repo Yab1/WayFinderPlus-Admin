7572d8ac664df24789bda3296be9abbef097663c
Yab1 <yabilisanu@gmail.com>
2023-04-23 01:16:17 +0300

    a function that controls user validation

---
+++ src/functions/authentication.js
@@ -0,0 +1,7 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth();
+
+export function authentication(user) {
+  // console.log(user);
+}
+// signInWithEmailAndPassword(auth);
