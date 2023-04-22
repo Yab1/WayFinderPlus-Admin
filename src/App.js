3c41268a04b41791b8e55c5675cc08d627d08bf4
Yab1 <yabilisanu@gmail.com>
2023-04-23 01:18:20 +0300

    rendered newly created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Navigation from "./components/Navigation/Navigation";
+import { useEffect, useState } from "react";
+// Components
+import Drawer from "./components/Drawer/Drawer";
+import POIEditor from "./pages/poiEditor";
+import Account from "./components/Account/Account";
+import LoginForm from "./components/Login/loginForm";
+
+// Hooks & Functions
+import useFetch from "./hooks/useFetch";
+import getCollectionData from "./firebase/connection";
 
 function App() {
+  const [logged, setLogged] = useState(false);
+  const handleLog = () => {
+    setLogged(!logged);
+  };
   return (
-    <div className="App">
-      <Navigation />
+    <div
+      className="App"
+      style={{ height: "100dvh", width: "100dvw", display: "flex" }}
+    >
+      {!logged ? (
+        <LoginForm handleLog={handleLog} />
+      ) : (
+        <>
+          <Account handleLog={handleLog} />
+          <Drawer />
+          <POIEditor />
+        </>
+      )}
     </div>
   );
 }
