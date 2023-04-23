41447af083f77280573d63e335bb0a989f6d5284
Yab1 <yabilisanu@gmail.com>
2023-04-24 00:54:42 +0300

    blank pages

---
+++ src/pages/eventController.js
@@ -0,0 +1,7 @@
+import StyledCard from "../shared/card";
+
+function EventController() {
+  return <StyledCard>EventController</StyledCard>;
+}
+
+export default EventController;
---
+++ src/pages/mapEditor.js
@@ -0,0 +1,7 @@
+import StyledCard from "../shared/card";
+
+function MapEditor() {
+  return <StyledCard>MapEditor</StyledCard>;
+}
+
+export default MapEditor;
