e6df5970a983e46bcb1b6c9f54ec4de5980051cd
Yab1 <yabilisanu@gmail.com>
2023-04-24 00:59:08 +0300

    Wrapped with context provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import AuthContextProvider from "./contexts/AuthContext.js";
+import BuildingsContextProvider from "./contexts/BuildingsContext.js";
+import CssBaseline from "@mui/material/CssBaseline";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <BuildingsContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BuildingsContextProvider>
   </React.StrictMode>
 );
