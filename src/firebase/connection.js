1ffe21553a4802de2904aca22e7dcc93d859d2cc
Yab1 <yabilisanu@gmail.com>
2023-04-24 00:53:01 +0300

    clear comments

--- src/firebase/connection.js
+++ src/firebase/connection.js
@@ -1,6 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyByUMrb32XM9dFUK1sMp_8W6hEm8PnF49s",
@@ -12,42 +10,4 @@ const firebaseConfig = {
 };
 
 // init firebase app
-const app = initializeApp(firebaseConfig);
-
-// init service
-const db = getFirestore(app);
-
-async function getCollectionData() {
-  // collection ref
-  const colRef = collection(db, "admins");
-
-  // get collection data
-  const snapshot = await getDocs(colRef);
-  const admins = snapshot.docs.map(admin => admin.data());
-}
-// const usrf = getCollectionData();
-
-const auth = getAuth();
-export function authentication(usr) {
-  createUserWithEmailAndPassword(auth, usr.email, usr.password)
-    .then(userCredential => {
-      const user = userCredential.user;
-      console.log(user);
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-    });
-}
-
-// export function authentication(usr) {
-//   console.log(usrf.email === usr.email);
-//   console.log(usrf.password === usr.password);
-
-//   signInWithEmailAndPassword(auth, usr.email, usr.password)
-//     .then(cred => console.log("user logged in:", cred.user))
-//     .catch(error => console.log(error.message));
-// }
-
-export default getCollectionData;
+export const app = initializeApp(firebaseConfig);
