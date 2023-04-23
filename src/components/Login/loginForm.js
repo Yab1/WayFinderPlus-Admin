5b84ac6a55c1605afbef3df515722938a2918940
Yab1 <yabilisanu@gmail.com>
2023-04-24 00:50:12 +0300

    hooked with a context to login and to put the data to localstorage

--- src/components/Login/loginForm.js
+++ src/components/Login/loginForm.js
@@ -1,110 +1,138 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-// import { authentication } from "../../functions/authentication.js";
-import { authentication } from "../../firebase/connection";
+import { AuthContext } from "../../contexts/AuthContext";
 
 //MUI Components
-import InputField from "../../shared/InputField";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
 
 //MUI Icons
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 function LoginForm({ handleLog }) {
-  const [user, setUser] = useState({ email: "", password: "" });
+  const [emailError, setEmailError] = useState(false);
+  const [passError, setPassError] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
   const handlePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-  const handleChange = e => {
-    const id = e.target.id;
-    const value = e.target.value;
-    id === "Email"
-      ? setUser(prevUser => ({
-          ...prevUser,
-          email: value,
-        }))
-      : setUser(prevUser => ({
-          ...prevUser,
-          password: value,
-        }));
-  };
-  const handleSubmit = e => {
-    handleLog();
-    authentication(user);
 
-    // setUser({ email: "", password: "" })
-  };
-
-  const inputs = [{ label: "Email", type: "text", value: user.email, key: 1 }];
   return (
-    <Dialog
-      open={true}
-      className="visuals"
-      sx={{ position: "absolute", zIndex: "tooltip" }}
-    >
-      <DialogContent
-        sx={{
-          width: 400,
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-        }}
-      >
-        {inputs.map(input => (
-          <InputField
-            input={input}
-            key={input.key}
-            handleChange={e => handleChange(e)}
-          />
-        ))}
-        <FormControl variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="password"
-            value={user.password}
-            onChange={e => handleChange(e)}
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handlePasswordVisibility}
-                  // onMouseDown={() => console.log("mouse down")}
-                  edge="end"
+    <AuthContext.Consumer>
+      {context => {
+        const { currentUser, userInput, errorMsg, handleChange, login } =
+          context;
+        const handleError = () => {
+          setEmailError(false);
+          setPassError(false);
+          if (errorMsg === "wrong-password") {
+            setPassError(!passError);
+          } else if (errorMsg !== "") {
+            setEmailError(!emailError);
+            setPassError(!passError);
+          }
+        };
+        return (
+          <Dialog
+            open={true}
+            className="visuals"
+            sx={{ position: "absolute", zIndex: "tooltip" }}
+          >
+            <DialogContent
+              sx={{
+                width: 400,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <Typography variant="h6" sx={{ mx: "auto", mb: 2 }}>
+                CNS-Admin-Login
+              </Typography>
+              {!currentUser && errorMsg !== "" && (
+                <Typography
+                  border={1}
+                  variant="body2"
+                  color="error"
+                  sx={{
+                    borderStyle: "dashed",
+                    borderRadius: 2,
+                    textAlign: "center",
+                    p: 1,
+                    mb: 3,
+                    transition: "all 1000ms ease-in",
+                  }}
                 >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          sx={{ px: 3, mx: "auto" }}
-          onClick={handleSubmit}
-        >
-          Login
-        </Button>
-      </DialogContent>
-    </Dialog>
+                  {errorMsg}
+                </Typography>
+              )}
+              <TextField
+                error={emailError}
+                id="email"
+                label="Email"
+                type="text"
+                value={userInput.email}
+                onChange={handleChange}
+                sx={{
+                  bg: "none",
+                }}
+              />
+              <FormControl variant="outlined">
+                <InputLabel
+                  error={passError}
+                  htmlFor="outlined-adornment-password"
+                >
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  error={passError}
+                  id="password"
+                  value={userInput.password}
+                  onChange={e => handleChange(e)}
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handlePasswordVisibility}
+                        edge="end"
+                      >
+                        {showPassword ? (
+                          <VisibilityOff color={passError ? "error" : ""} />
+                        ) : (
+                          <Visibility color={passError ? "error" : ""} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button
+                variant="contained"
+                sx={{ px: 3, mx: "auto" }}
+                onClick={() => {
+                  handleError();
+                  login();
+                }}
+              >
+                Login
+              </Button>
+            </DialogContent>
+          </Dialog>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 }
 
@@ -113,22 +141,3 @@ LoginForm.prototype = {
 };
 
 export default LoginForm;
-
-{
-  /* <Stack
-  direction="row"
-  spacing={12}
-  // justifyContent="center"
-  alignItems="center"
->
-  <FormGroup>
-    <FormControlLabel
-      control={<Checkbox defaultChecked />}
-      label="Remember Me"
-    />
-  </FormGroup>
-  <Link href="#" underline="none" sx={{ fontSize: 13 }}>
-    Forgot Password
-  </Link>
-</Stack> */
-}
