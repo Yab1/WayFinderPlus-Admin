299fa75e6fd97c734df1551d315a167232f9c7b5
Yab1 <yabilisanu@gmail.com>
2023-04-24 00:58:22 +0300

    added react route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import { useEffect, useState } from "react";
 // Components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Drawer from "./components/Drawer/Drawer";
-import POIEditor from "./pages/poiEditor";
 import Account from "./components/Account/Account";
 import LoginForm from "./components/Login/loginForm";
+import Dataset from "./pages/dataset";
+import EventController from "./pages/eventController";
+import MapEditor from "./pages/mapEditor";
+import POIEditor from "./pages/poiEditor";
 
-// Hooks & Functions
-import useFetch from "./hooks/useFetch";
-import getCollectionData from "./firebase/connection";
+// Hook, Contexts & Functions
+import { AuthContext } from "./contexts/AuthContext.js";
 
 function App() {
-  const [logged, setLogged] = useState(false);
-  const handleLog = () => {
-    setLogged(!logged);
-  };
   return (
     <div
       className="App"
       style={{ height: "100dvh", width: "100dvw", display: "flex" }}
     >
-      {!logged ? (
-        <LoginForm handleLog={handleLog} />
-      ) : (
-        <>
-          <Account handleLog={handleLog} />
-          <Drawer />
-          <POIEditor />
-        </>
-      )}
+      <AuthContext.Consumer>
+        {context => {
+          const { logged } = context;
+          return (
+            <>
+              {!logged ? (
+                <LoginForm />
+              ) : (
+                <>
+                  <BrowserRouter>
+                    <Drawer />
+                    <Account />
+                    <Routes>
+                      <Route exact path="/" element={<Dataset />} />
+                      <Route
+                        path="/EventController"
+                        element={<EventController />}
+                      />
+                      <Route path="/MapEditor" element={<MapEditor />} />
+                      <Route path="/POIEditor" element={<POIEditor />} />
+                    </Routes>
+                  </BrowserRouter>
+                </>
+              )}
+            </>
+          );
+        }}
+      </AuthContext.Consumer>
     </div>
   );
 }
