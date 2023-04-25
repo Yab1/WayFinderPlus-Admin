5b6f5933c687dae64945de49c722c8eca74b4ca3
Yab1 <yabilisanu@gmail.com>
2023-04-25 18:04:23 +0300

    Stayled table rows

--- src/shared/row.js
+++ src/shared/row.js
@@ -1,25 +1,69 @@
+import { useState } from "react";
+import Popup from "./dialog";
+
+// MUI Components, Colors and Icons
+import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { grey } from "@mui/material/colors";
 
-export default function Row({ building, deleteData }) {
+export default function Row({ building, id, deleteData }) {
+  const [open, setOpen] = useState(false);
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(even)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  const style = {
+    color: grey[500],
+  };
+
+  const handleClose = () => setOpen(!open);
   return (
-    <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+    <StyledTableRow sx={{ "& > *": { pt: 0 } }}>
+      <TableCell align="left">{building.buildingNumber}</TableCell>
+      <TableCell align="left">{building.buildingCategory}</TableCell>
+      <TableCell
+        align="left"
+        sx={building.buildingName === "unnamed" ? style : null}
+      >
+        {building.buildingName}
+      </TableCell>
+      <TableCell
+        align="left"
+        sx={building.buildingDescription === "No Data" ? style : null}
+      >
+        {building.buildingDescription}
+      </TableCell>
+      <TableCell align="left">{building.created_at}</TableCell>
       <TableCell>
         <IconButton
           aria-label="delete row"
           size="small"
-          onClick={() => deleteData(building.id)}
+          onClick={() => setOpen(!open)}
         >
-          <DeleteOutlineIcon />
+          <DeleteOutlineIcon
+            sx={{
+              "&:hover": { color: "error.main" },
+              color: open && "error.main",
+            }}
+          />
         </IconButton>
+        <Popup
+          open={open}
+          building={building}
+          handleClose={handleClose}
+          deleteData={deleteData}
+        ></Popup>
       </TableCell>
-      <TableCell align="right">{building.buildingNumber}</TableCell>
-      <TableCell align="right">{building.buildingCategory}</TableCell>
-      <TableCell align="right">{building.buildingName}</TableCell>
-      <TableCell align="right">{building.buildingDescription}</TableCell>
-      <TableCell align="right">{building.created_at}</TableCell>
-    </TableRow>
+    </StyledTableRow>
   );
 }
