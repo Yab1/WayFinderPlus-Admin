4c75488bc9567c5e3305aa79cdc277f0f4c241b8
Yab1 <yabilisanu@gmail.com>
2023-06-02 14:34:30 +0300

    Added the MapContext

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import "./index.css";
 import App from "./App";
 import AuthContextProvider from "./contexts/AuthContext.js";
 import BuildingsContextProvider from "./contexts/BuildingsContext.js";
+import MapContextProvider from "./contexts/MapContext.js";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <BuildingsContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </BuildingsContextProvider>
+    <MapContextProvider>
+      <BuildingsContextProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </BuildingsContextProvider>
+    </MapContextProvider>
   </React.StrictMode>
 );
