4afcd7486ee1fb5e61aea753bf1e6f67fdb72e9e
Yab1 <yabilisanu@gmail.com>
2023-06-24 14:11:50 +0300

    Implement pop-up window with form for capturing event information

--- src/shared/eventEditor.js
+++ src/shared/eventEditor.js
@@ -18,23 +18,34 @@ import MuiAlert from "@mui/material/Alert";
 // MUI Icons
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import SendIcon from "@mui/icons-material/Send";
-import { ElectricalServicesOutlined } from "@mui/icons-material";
 
 const Alert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 function EventEditor({ event, handleEvent, clickedMarkerId }) {
-  const [open, setOpen] = useState(false);
-  const [error, setError] = useState({
-    location: false,
-    category: false,
-    startDate: false,
-    endDate: false,
+  const [snack, setSnack] = useState({
+    open: false,
+    vertical: "bottom",
+    horizontal: "left",
+    severity: "",
+    message: "",
   });
+  const errorMessage = {
+    loc: "Please choose a location on the map.",
+    cat: "Please select a category for the event.",
+    date: "Please choose a valid date for the event.",
+    locCat: "Please choose a location and select a category for the event.",
+    locDate: "Please choose a location and choose a valid date for the event.",
+    catDate: "Please select a category and choose a valid date for the event.",
+    all: "Incomplete information. Please fill in all required fields.",
+    success: "Event saved successfully!",
+  };
 
   // Context
   const { buildingsData } = useContext(MapContext);
+
+  const { vertical, horizontal, open, severity, message } = snack;
 
   let markerArr = [];
   if (buildingsData !== []) {
@@ -61,60 +72,116 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
     }));
   }, [building.geoHash]);
 
-  const handleError = () => {
-    setError({
-      location: true,
-      category: false,
-      startDate: false,
-      endDate: false,
+  const handleClick = (newSnack) => {
+    setSnack((prevSnack) => ({ ...prevSnack, ...newSnack, open: true }));
+  };
+  const handleClose = () => {
+    setSnack({ ...snack, open: false });
+  };
+  const reset = () => {
+    handleEvent({
+      geoHash: "",
+      startDate: "",
+      endDate: "",
+      eventCategory: "",
+      eventName: "",
+      eventDescription: "",
     });
-    event.geoHash === false &&
-      setError((prevError) => ({
-        ...prevError,
-        location: !prevError.location,
-      }));
-    event.eventCategory === "" &&
-      setError((prevError) => ({
-        ...prevError,
-        category: !prevError.category,
-      }));
-    event.startDate === "" &&
-      setError((prevError) => ({
-        ...prevError,
-        startDate: !prevError.startDate,
-      }));
-    event.endDate === "" &&
-      setError((prevError) => ({
-        ...prevError,
-        endDate: !prevError.endDate,
-      }));
   };
 
-  const handleClick = () => {
-    setOpen(true);
-  };
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
   return (
     <EventContext.Consumer>
       {(context) => {
         const { addData } = context;
         const middleMan = () => {
-          if (event.geoHash && event.eventCategory) {
-            building.buildingNumber = "";
-            console.log(event);
-            // addData(event);
-            handleEvent({
-              geoHash: "",
-              eventCategory: "",
-              eventName: "",
-              eventDescription: "",
+          if (
+            event.geoHash &&
+            event.eventCategory &&
+            event.startDate &&
+            event.endDate
+          ) {
+            // All conditions are true
+            handleClick({
+              severity: "success",
+              message: errorMessage.success,
             });
+            addData(event);
+            reset();
+          } else {
+            if (
+              !event.geoHash &&
+              !event.eventCategory &&
+              !event.startDate &&
+              !event.endDate
+            ) {
+              // All conditions are false
+              handleClick({
+                severity: "error",
+                message: errorMessage.all,
+              });
+            } else if (
+              !event.geoHash &&
+              event.eventCategory &&
+              event.startDate &&
+              event.endDate
+            ) {
+              // Only location is false
+              handleClick({
+                severity: "error",
+                message: errorMessage.loc,
+              });
+            } else if (
+              event.geoHash &&
+              !event.eventCategory &&
+              event.startDate &&
+              event.endDate
+            ) {
+              // Only category is false
+              handleClick({
+                severity: "error",
+                message: errorMessage.cat,
+              });
+            } else if (
+              event.geoHash &&
+              event.eventCategory &&
+              (!event.startDate || !event.endDate)
+            ) {
+              // Only date is false
+              handleClick({
+                severity: "error",
+                message: errorMessage.date,
+              });
+            } else if (
+              !event.geoHash &&
+              event.eventCategory &&
+              (!event.startDate || !event.endDate)
+            ) {
+              // Only location and date are false
+              handleClick({
+                severity: "error",
+                message: errorMessage.locDate,
+              });
+            } else if (
+              event.geoHash &&
+              !event.eventCategory &&
+              (!event.startDate || !event.endDate)
+            ) {
+              // Only category and date are false
+              handleClick({
+                severity: "error",
+                message: errorMessage.catDate,
+              });
+            } else if (
+              !event.geoHash &&
+              !event.eventCategory &&
+              (event.startDate || event.endDate)
+            ) {
+              // Only location and category are false
+              handleClick({
+                severity: "error",
+                message: errorMessage.locCat,
+              });
+            }
           }
         };
         return (
@@ -130,17 +197,17 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
                 right: "5%",
                 width: {
                   xs: "60%",
-                  sm: "60%",
-                  md: "40%",
+                  sm: "45%",
+                  md: "35%",
                   lg: "25%",
-                  xl: "25%",
+                  xl: "20%",
                 },
                 display: "flex",
                 flexDirection: "column",
               }}
             >
               <Stack spacing={3}>
-                <FormControl disabled variant="standard">
+                <FormControl disabled variant="standard" required>
                   <InputLabel htmlFor="component-disabled">
                     Event Location
                   </InputLabel>
@@ -168,12 +235,11 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
                 />
                 <TextField
                   id="eventCategory"
-                  error={error.category}
                   required
                   fullWidth
                   variant="standard"
                   select
-                  label="Building Category"
+                  label="Event Category"
                   defaultValue=""
                   value={event.eventCategory}
                   onChange={(e) =>
@@ -193,6 +259,50 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
                     </MenuItem>
                   ))}
                 </TextField>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    color: "#757575",
+                  }}
+                >
+                  <Stack>
+                    <label htmlFor="startDate">Start Date *</label>
+                    <input
+                      type="date"
+                      id="startDate"
+                      value={event.startDate}
+                      style={{
+                        width: "100%",
+                        padding: "0.5em",
+                      }}
+                      onChange={(e) => {
+                        handleEvent((prevEvent) => ({
+                          ...prevEvent,
+                          startDate: e.target.value,
+                        }));
+                      }}
+                    />
+                  </Stack>
+                  <Stack>
+                    <label htmlFor="endDate">End Date *</label>
+                    <input
+                      type="date"
+                      value={event.endDate}
+                      id="endDate"
+                      style={{
+                        width: "100%",
+                        padding: "0.5em",
+                      }}
+                      onChange={(e) => {
+                        handleEvent((prevEvent) => ({
+                          ...prevEvent,
+                          endDate: e.target.value,
+                        }));
+                      }}
+                    ></input>
+                  </Stack>
+                </Box>
                 <TextField
                   id="eventDescription"
                   label="Building Description"
@@ -208,22 +318,10 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
                     }))
                   }
                 />
-
-                <Button
-                  id="pictureInput"
-                  variant="outlined"
-                  component="label"
-                  endIcon={<PhotoCamera />}
-                >
-                  Upload
-                  <input hidden accept="image/*" multiple type="file" />
-                </Button>
               </Stack>
               <Button
                 onClick={() => {
-                  handleError();
                   middleMan();
-                  handleClick();
                 }}
                 variant="contained"
                 endIcon={<SendIcon />}
@@ -232,16 +330,18 @@ function EventEditor({ event, handleEvent, clickedMarkerId }) {
                 Submit
               </Button>
               <Snackbar
+                anchorOrigin={{ vertical, horizontal }}
                 open={open}
                 autoHideDuration={6000}
+                sx={{ marginBottom: "1.4em", marginLeft: "3.5em" }}
                 onClose={handleClose}
               >
                 <Alert
                   onClose={handleClose}
-                  severity="success"
+                  severity={severity}
                   sx={{ width: "100%" }}
                 >
-                  This is a success message!
+                  {message}
                 </Alert>
               </Snackbar>
             </Box>
