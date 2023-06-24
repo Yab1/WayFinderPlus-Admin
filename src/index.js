fd570e409725ce4ca7e0f76873e117a5b72fe239
Yab1 <yabilisanu@gmail.com>
2023-06-24 14:14:28 +0300

    added new context

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import AuthContextProvider from "./contexts/AuthContext.js";
-import BuildingsContextProvider from "./contexts/BuildingsContext.js";
 import MapContextProvider from "./contexts/MapContext.js";
 import EventContextProvider from "./contexts/EventContext.js";
+import BucketContextProvider from "./contexts/BucketContext";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,11 +14,11 @@ root.render(
     <CssBaseline />
     <MapContextProvider>
       <EventContextProvider>
-        <BuildingsContextProvider>
+        <BucketContextProvider>
           <AuthContextProvider>
             <App />
           </AuthContextProvider>
-        </BuildingsContextProvider>
+        </BucketContextProvider>
       </EventContextProvider>
     </MapContextProvider>
   </React.StrictMode>
