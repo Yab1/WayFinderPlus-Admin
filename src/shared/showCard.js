c1e36e8ba8532886ecfb6037665ad0cca50c6194
Yab1 <yabilisanu@gmail.com>
2023-06-24 14:18:57 +0300

    final cleanup

--- src/shared/showCard.js
+++ src/shared/showCard.js
@@ -1,5 +1,6 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MapContext } from "../contexts/MapContext";
+import { EventContext } from "../contexts/EventContext";
 import Popup from "./dialog";
 import Thumbnail from "../assets/Thumbnail image.jpeg";
 import Card from "@mui/material/Card";
@@ -10,10 +11,15 @@ import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
-import { red } from "@mui/material/colors";
+import Collapse from "@mui/material/Collapse";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
+import { green } from "@mui/material/colors";
 
 import CloseIcon from "@mui/icons-material/Close";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EventIcon from "@mui/icons-material/Event";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 export default function ShowCard({
   clickedMarkerId,
@@ -22,20 +28,28 @@ export default function ShowCard({
   handleMarkerClick,
 }) {
   const { deleteData } = useContext(MapContext);
+  const { events, deleteEvents } = useContext(EventContext);
   const [open, setOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleClose = () => setOpen(!open);
 
   let markerArr = [];
+  let filteredEvent = [];
   if (buildingsData !== []) {
     markerArr = buildingsData.filter(
       (building) => building.id === clickedMarkerId
+    );
+    filteredEvent = events.filter(
+      (event) => event.geoHash === markerArr[0].geoHash
     );
   }
   let building = {};
 
   if (markerArr[0]) {
     const {
+      url,
+      geoHash,
       buildingNumber,
       buildingName,
       buildingDescription,
@@ -44,6 +58,8 @@ export default function ShowCard({
     } = markerArr[0];
     building = {
       id: clickedMarkerId,
+      url,
+      geoHash,
       buildingNumber,
       buildingName,
       buildingDescription,
@@ -71,7 +87,7 @@ export default function ShowCard({
       >
         <CardHeader
           avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
               {building.buildingNumber}
             </Avatar>
           }
@@ -86,7 +102,7 @@ export default function ShowCard({
         <CardMedia
           component="img"
           height="194"
-          image={Thumbnail}
+          image={building.url ? building.url : Thumbnail}
           alt="Thumbnail"
         />
         <CardContent>
@@ -94,11 +110,62 @@ export default function ShowCard({
             {building.buildingDescription}
           </Typography>
         </CardContent>
-        <CardActions disableSpacing>
+        <CardActions disableSpacing sx={{ justifyContent: "space-between" }}>
+          {filteredEvent[0] && (
+            <IconButton
+              aria-label="event available"
+              onClick={() => setExpanded(!expanded)}
+            >
+              <EventIcon color="success" />
+            </IconButton>
+          )}
+          {!filteredEvent[0] && (
+            <Tooltip title="No event available" placement="bottom-start">
+              <span>
+                <IconButton aria-label="event not available" disabled>
+                  <CalendarTodayIcon color="disabled" />
+                </IconButton>
+              </span>
+            </Tooltip>
+          )}
           <IconButton aria-label="delete" onClick={handleClose}>
-            <DeleteIcon />
+            <DeleteIcon color="error" />
           </IconButton>
         </CardActions>
+        {filteredEvent[0] && (
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {filteredEvent[0].eventName}
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {`${filteredEvent[0].startDate} - ${filteredEvent[0].endDate}`}
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {filteredEvent[0].eventCategory}
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {filteredEvent[0].eventDescription}
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{ width: "100%", ml: "25%" }}
+              onClick={() =>
+                filteredEvent[0] && deleteEvents(filteredEvent[0].id)
+              }
+            >
+              <Button variant="contained" color="error">
+                Delete Event
+              </Button>
+            </CardActions>
+          </Collapse>
+        )}
         <Popup
           open={open}
           building={building}
