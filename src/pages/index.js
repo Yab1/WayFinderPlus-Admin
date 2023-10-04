ac5e80fbc1acc3c659c665f5003b6efaf751e8c6
Yab1 <yabilisanu@gmail.com>
2023-10-04 19:04:37 +0300

    Changed Routing method

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as MyMap } from "./MyMap";
+export { default as TabularData } from "./TabularData";
+export { default as MapDataEditor } from "./MapDataEditor";
+export { default as EventDataEditor } from "./EventDataEditor";
