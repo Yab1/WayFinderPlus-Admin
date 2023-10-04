ed63fe728dc73f4d77295f131d306621ac537da2
Yab1 <yabilisanu@gmail.com>
2023-10-04 19:04:26 +0300

    Middleware logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { authReducer } from "@/layouts";
+import { uiReducer } from "@/slices";
 
 const store = configureStore({
-  reducer: {},
-  middleware: [logger],
+  reducer: {
+    auth: authReducer,
+    ui: uiReducer,
+  },
+  middleware: [thunk, logger],
 });
 
 export default store;
