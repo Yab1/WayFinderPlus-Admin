475bbd20b7482423abccf9044d02c7d1db027471
Yab1 <yabilisanu@gmail.com>
2023-10-04 19:02:17 +0300

    Changed Routing method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
+//React,Redux,Router
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 // Components
-import { Routes, Route } from "react-router-dom";
-import Drawer from "./components/Drawer/Drawer";
-import MobileSpeedDial from "./components/Drawer/MobileSpeedDial";
-import Account from "./components/Account/Account";
-import LoginForm from "./components/Login/LoginForm";
-
-// pages
-import StreetView from "./pages/streetView";
-import Dataset from "./pages/dataset";
-import EventController from "./pages/eventController";
-import MapEditor from "./pages/mapEditor";
-
-// Hook, Contexts & Functions
-import { AuthContext } from "./contexts/AuthContext.js";
+import { Auth, Dashboard, checkLocalStorage } from "@/layouts";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkLocalStorage());
+  }, []);
+
   return (
     <div
       className="App"
@@ -25,33 +23,11 @@ function App() {
         flexDirection: { xs: "column", sm: "column", md: "row", lg: "row" },
       }}
     >
-      <Drawer />
-      <Account />
+      <Auth />
       <Routes>
-        <Route exact path="/" element={<StreetView />} />
-        {/* <Route exact path="/wayfinder" element={<StreetView />} /> */}
-        <Route exact path="/Dataset" element={<Dataset />} />
-        <Route path="/EventController" element={<EventController />} />
-        <Route path="/MapEditor" element={<MapEditor />} />
-        <Route path="/LoginForm" element={<LoginForm />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/dashboard/my-map" replace />} />
       </Routes>
-      <MobileSpeedDial />
-
-      {/* <AuthContext.Consumer>
-        {(context) => {
-          const { logged } = context;
-          return (
-            // <>
-            //   {!logged ? (
-            //     <LoginForm />
-            //   ) : (
-            //     <>
-            //     </>
-            //   )}
-            // </>
-          );
-        }}
-      </AuthContext.Consumer> */}
     </div>
   );
 }
