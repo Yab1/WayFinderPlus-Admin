6f99ece805bb7154bc6503dcbe9196544f1bcace
Yab1 <yabilisanu@gmail.com>
2023-10-06 01:12:47 +0300

    Import allies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { store } from "@/redux";
 import { Provider } from "react-redux";
+import { store } from "@/redux";
+import "@/index.css";
+import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
