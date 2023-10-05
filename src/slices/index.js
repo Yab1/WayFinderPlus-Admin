86637af7833e9e7c2b96b3d4753094c7f950131e
Yab1 <yabilisanu@gmail.com>
2023-10-06 01:05:13 +0300

    Redux: map data controller

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,2 +1,6 @@
 export { default as uiReducer } from "./uiSlice";
 export * from "./uiSlice";
+export { default as mapBoxReducer } from "./mapBoxSlice";
+export * from "./mapBoxSlice";
+export { default as buildingsDataReducer } from "./buildingDataSlice";
+export * from "./buildingDataSlice";
