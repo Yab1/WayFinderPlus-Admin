9ce6658413c796820b3604433b407e1f7a9dc208
Yab1 <yabilisanu@gmail.com>
2023-10-06 01:10:51 +0300

    Removed contexts

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import AuthContextProvider from "./contexts/AuthContext.js";
-import MapContextProvider from "./contexts/MapContext.js";
-import EventContextProvider from "./contexts/EventContext.js";
-import BucketContextProvider from "./contexts/BucketContext";
-import CssBaseline from "@mui/material/CssBaseline";
 import { store } from "@/redux";
 import { Provider } from "react-redux";
 
@@ -16,16 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <CssBaseline />
-        <MapContextProvider>
-          <EventContextProvider>
-            <BucketContextProvider>
-              <AuthContextProvider>
-                <App />
-              </AuthContextProvider>
-            </BucketContextProvider>
-          </EventContextProvider>
-        </MapContextProvider>
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>
