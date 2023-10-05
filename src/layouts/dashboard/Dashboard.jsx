be19bcc65de235bb8c9c1098abee3afcbe5bd021
Yab1 <yabilisanu@gmail.com>
2023-10-06 01:00:59 +0300

    Redux: Dispatched fetchThunk action.

--- src/layouts/dashboard/Dashboard.jsx
+++ src/layouts/dashboard/Dashboard.jsx
@@ -1,16 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { SideNav, AccountMenu } from "@/widgets/layout";
 import routes from "@/routes";
+import { fetchData } from "@/slices";
+import { useDispatch } from "react-redux";
+import { StyleController } from "@/widgets/layout";
 
 function Dashboard() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
   return (
     <Fragment>
       <SideNav />
       <AccountMenu />
+      <StyleController />
       <Routes>
-        {routes.map(({ path, element }) => (
-          <Route exact path={path} element={element} />
+        {routes.map(({ name, path, element }) => (
+          <Route key={name} exact path={path} element={element} />
         ))}
       </Routes>
     </Fragment>
