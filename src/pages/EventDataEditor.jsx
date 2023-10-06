e13a11e4037e0396c676581f29c4d3bd0289097b
Yab1 <yabilisanu@gmail.com>
2023-10-06 17:13:35 +0300

    MapBox: rendered map

--- src/pages/EventDataEditor.jsx
+++ src/pages/EventDataEditor.jsx
@@ -1,5 +1,48 @@
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StyleController } from "@/widgets/layout";
+import { createMarker } from "@/functions";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 function EventDataEditor() {
-  return <div>EventDataEditor</div>;
+  const { metaData, mapStyle, layer } = useSelector((state) => state.mapBox);
+  const { data } = useSelector((state) => state.buildingData);
+  const dispatch = useDispatch();
+
+  function dispatcher(markerData) {
+    //  dispatch(setClickedMarker(markerData));
+  }
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN;
+    const mapBox = new mapboxgl.Map({ ...metaData });
+    mapBox.setStyle("mapbox://styles/mapbox/" + mapStyle);
+
+    const nav = new mapboxgl.NavigationControl();
+    mapBox.addControl(nav, "bottom-right");
+
+    mapBox.on("style.load", () => {
+      createMarker(mapBox, data, dispatcher);
+
+      const layers = mapBox.getStyle().layers;
+      const labelLayerId = layers.find(
+        (layer) => layer.type === "symbol" && layer.layout["text-field"]
+      ).id;
+      mapBox.addLayer(layer, labelLayerId);
+    });
+
+    return () => {
+      mapBox.remove();
+    };
+  }, [mapStyle]);
+
+  return (
+    <Fragment>
+      <StyleController />
+      <div id="mapBox"></div>
+    </Fragment>
+  );
 }
 
 EventDataEditor.displayName = "/src/pages/EventDataEditor.jsx";
