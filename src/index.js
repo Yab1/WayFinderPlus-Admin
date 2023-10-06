454334b92c3efeec42b29ba55d6749e74db866c8
Yab1 <yabilisanu@gmail.com>
2023-10-06 23:15:56 +0300

    Moved mapbox gl css style to the top

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "@/redux";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "@/index.css";
 import App from "@/App";
 
