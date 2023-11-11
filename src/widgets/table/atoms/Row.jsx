3cbea9569a0ded68f7f247dfe9669e9f35a4863b
Yab1 <yabilisanu@gmail.com>
2023-11-11 21:08:41 +0300

    Added a ternary to check if the data is not empty.

--- src/widgets/table/atoms/Row.jsx
+++ src/widgets/table/atoms/Row.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { markDataForDeletion } from "@/slices";
 import { styled } from "@mui/material/styles";
@@ -24,64 +25,67 @@ function Row() {
   const { data, dataToDelete } = useSelector((state) => state.buildingData);
   const dispatch = useDispatch();
 
-  const renderRow = data.map(
-    ({
-      id,
-      buildingNumber,
-      buildingCategory,
-      buildingName,
-      buildingDescription,
-      created_at,
-    }) => (
-      <StyledTableRow key={id} sx={{ "& > *": { pt: 0 } }}>
-        <TableCell align="left" width="10%" sx={{ fontSize: 12 }}>
-          {buildingNumber}
-        </TableCell>
-        <TableCell align="left" width="14%" sx={{ fontSize: 12 }}>
-          {buildingCategory}
-        </TableCell>
-        <TableCell
-          align="left"
-          sx={
-            (buildingName === "unnamed"
-              ? style
-              : { textTransform: "capitalize" },
-            { fontSize: 12 })
-          }
-          width="15%"
-        >
-          {buildingName}
-        </TableCell>
-
-        <TableCell
-          align="left"
-          width="45%"
-          sx={buildingDescription === "No Data" ? style : { fontSize: 12 }}
-        >
-          {buildingDescription}
-        </TableCell>
-        <TableCell align="center" sx={{ fontSize: 12 }}>
-          {created_at}
-        </TableCell>
-        <TableCell>
-          <IconButton
-            aria-label="delete the current row"
-            size="small"
-            onClick={() => dispatch(markDataForDeletion(id))}
+  const renderRow =
+    data &&
+    data.map(
+      ({
+        id,
+        buildingNumber,
+        buildingCategory,
+        buildingName,
+        buildingDescription,
+        created_at,
+      }) => (
+        <StyledTableRow key={id} sx={{ "& > *": { pt: 0 } }}>
+          <TableCell align="left" width="10%" sx={{ fontSize: 12 }}>
+            {buildingNumber}
+          </TableCell>
+          <TableCell align="left" width="14%" sx={{ fontSize: 12 }}>
+            {buildingCategory}
+          </TableCell>
+          <TableCell
+            align="left"
+            sx={
+              (buildingName === "unnamed"
+                ? style
+                : { textTransform: "capitalize" },
+              { fontSize: 12 })
+            }
+            width="15%"
           >
-            <DeleteOutlineIcon
-              sx={{
-                "&:hover": { color: "error.main" },
-                color: dataToDelete.id === id ? "error.main" : "",
-              }}
-            />
-          </IconButton>
-        </TableCell>
-      </StyledTableRow>
-    )
-  );
+            {buildingName}
+          </TableCell>
 
-  return renderRow;
+          <TableCell
+            align="left"
+            width="45%"
+            sx={buildingDescription === "No Data" ? style : { fontSize: 12 }}
+          >
+            {buildingDescription}
+          </TableCell>
+          <TableCell align="center" sx={{ fontSize: 12 }}>
+            {created_at}
+          </TableCell>
+          <TableCell>
+            <IconButton
+              aria-label="delete the current row"
+              size="small"
+              onClick={() => dispatch(markDataForDeletion(id))}
+            >
+              <DeleteOutlineIcon
+                sx={{
+                  "&:hover": { color: "error.main" },
+                  color:
+                    dataToDelete && dataToDelete.id === id ? "error.main" : "",
+                }}
+              />
+            </IconButton>
+          </TableCell>
+        </StyledTableRow>
+      )
+    );
+
+  return <Fragment>{renderRow}</Fragment>;
 }
 
 Row.displayName = "/src/widgets/table/atoms/Row.jsx";
