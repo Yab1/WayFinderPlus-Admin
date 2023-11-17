a389db205949532430c1a212a55eb1e2cd9492d8
Yab1 <yabilisanu@gmail.com>
2023-11-18 00:03:11 +0300

    Added WelcomePage

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@
 import { Routes, Route } from "react-router-dom";
 
 // Components
-import { Admin } from "@/layouts";
-import { VisitorFeatures } from "./features";
+import { WelcomePage } from "@/layouts";
+// import { VisitorFeatures } from "./features";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        height: "100dvh",
-        width: "100dvw",
-        display: "flex",
-        flexDirection: { xs: "column", sm: "column", md: "row", lg: "row" },
-      }}
-    >
-      <Routes>
-        {true ? (
-          <Route path="/wayfinder/*" element={<Admin />} />
-        ) : (
-          <Route exact path={"visiter*"} element={<VisitorFeatures />} />
-        )}
-      </Routes>
-    </div>
+    <WelcomePage />
+    // <div
+    //   className="App"
+    //   style={{
+    //     height: "100dvh",
+    //     width: "100dvw",
+    //     display: "flex",
+    //     flexDirection: { xs: "column", sm: "column", md: "row", lg: "row" },
+    //   }}
+    // >
+    //   <Routes>
+    //     {true ? (
+    //       <Route path="/wayfinder/*" element={<Admin />} />
+    //     ) : (
+    //       <Route exact path={"visiter*"} element={<VisitorFeatures />} />
+    //     )}
+    //   </Routes>
+    // </div>
   );
 }
 
