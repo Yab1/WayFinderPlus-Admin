7be921f76b51a2b3714c9ecd645f1349a535a589
Yab1 <yabilisanu@gmail.com>
2023-11-18 00:02:57 +0300

    Styled WelcomePage layout

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import BuildingRoute from "@/assets/3d-map.svg";
+import RouteLines from "@/assets/route-visual.svg";
+
+const Images = {
+  BuildingRoute,
+  RouteLines,
+};
+
+export default Images;
