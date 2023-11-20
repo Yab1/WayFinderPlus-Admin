9b89556b902799a91b2bcaf3b0d78a780cc7d4a8
Yab1 <yabilisanu@gmail.com>
2023-11-20 21:54:54 +0300

    Removed unnecessary files

--- src/features/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Auth } from "./auth/Auth";
-export { default as AdminFeatures } from "./admin/AdminFeatures";
-export { default as VisitorFeatures } from "./visitor/VisitorFeatures";
