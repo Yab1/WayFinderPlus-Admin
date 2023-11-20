b912989a7e4e2e56541a10f641fa0c7836cc3c08
Yab1 <yabilisanu@gmail.com>
2023-11-20 21:32:02 +0300

    Added Logo

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import BuildingRoute from "@/assets/3d-map.svg";
 import RouteLines from "@/assets/route-visual.svg";
+import Logo from "@/assets/wayfinder-plus-logo.png";
+import Thumbnail from "@/assets/Thumbnail.jpeg";
 
-const Images = {
+const Images = Object.freeze({
   BuildingRoute,
   RouteLines,
-};
+  Logo,
+  Thumbnail,
+});
 
 export default Images;
