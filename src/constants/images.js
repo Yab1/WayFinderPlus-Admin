89671e4b0d4e117a025876eb5eb1ae5eb154c374
Yab1 <yabilisanu@gmail.com>
2023-11-21 21:24:22 +0300

    Renamed

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import BuildingRoute from "@/assets/3d-map.svg";
 import RouteLines from "@/assets/route-visual.svg";
-import Logo from "@/assets/wayfinder-plus-logo.png";
+import Logo from "@/assets/wayfinder_logo.png";
 import Thumbnail from "@/assets/Thumbnail.jpeg";
 
 const Images = Object.freeze({
