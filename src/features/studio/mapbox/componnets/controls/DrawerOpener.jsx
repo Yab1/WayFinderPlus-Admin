a7086b138626de778c475e23d244f682150fafa0
Yab1 <yabilisanu@gmail.com>
2023-11-24 14:33:25 +0300

    Implemented a Button to control drawer visibility

---
+++ src/features/studio/mapbox/componnets/controls/DrawerOpener.jsx
@@ -0,0 +1,32 @@
+import { IconButton } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleDrawer } from "@/redux/slices";
+import { StyledButton } from "@/features/studio/mapbox/atoms";
+
+function DrawerOpener() {
+  const { isDrawerOpen } = useSelector((state) => state.ui);
+  const { newBounds } = useSelector((state) => state.mapBox);
+  const dispatch = useDispatch();
+
+  return (
+    <StyledButton
+      open={isDrawerOpen}
+      sx={{ display: newBounds ? "" : "none", top: 10, left: 10 }}
+    >
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={() => dispatch(toggleDrawer(true))}
+        sx={{ ...(isDrawerOpen && { display: "none" }) }}
+      >
+        <Menu sx={{ color: "#333333" }} />
+      </IconButton>
+    </StyledButton>
+  );
+}
+
+DrawerOpener.displayName =
+  "/src/features/studio/componnets/controls/DrawerOpener.jsx";
+
+export default DrawerOpener;
