b4e19b1aba7ca136f7f9a8fee15b71754bb7ba06
Yab1 <yabilisanu@gmail.com>
2023-11-24 14:38:28 +0300

    Refactored file structure for better organization.

---
+++ src/features/studio/mapbox/componnets/controls/index.js
@@ -0,0 +1,4 @@
+export { default as DrawControl } from "./DrawControl";
+export { default as DrawerOpener } from "./DrawerOpener";
+export { default as GeocoderControl } from "./GeocoderControl";
+export { default as StyleControl } from "./StyleControl";
