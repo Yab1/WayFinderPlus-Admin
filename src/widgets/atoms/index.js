5df1867ef5061a9f20d573bed3894dde147e57cd
Yab1 <yabilisanu@gmail.com>
2023-11-24 14:39:35 +0300

    Removed Tabs

--- src/widgets/atoms/index.js
+++ src/widgets/atoms/index.js
@@ -1,4 +1,3 @@
-export { default as Tabs } from "../../features/studio/widgets/sideNav/Tabs";
 export { default as StyledTextField } from "./StyledTextField";
 export { default as StyledInputLabel } from "./StyledInputLabel";
 export { default as StyledFormControl } from "./StyledFormControl";
